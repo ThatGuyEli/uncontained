f5e5326dc38610a1e578433f6e2388f7788a955c
Elijah Lieu <elijahlieu@gmail.com>
2020-11-16 11:37:09 -0500

    refactoring

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './css/App.css';
 
 // Component Imports
-import Game from './components/Game.js';
+import Level from './components/Level.js';
 
 class App extends Component {
   render() {
     return (
       // This is where the App is stored.
-      <Game level='1' />
+      <Level id='1' />
     );
   }
 }
